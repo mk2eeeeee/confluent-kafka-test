e0e7a7af3764a2ab8b7b38de5a84db110c8469f6
Sangwoo <eicpein@gmail.com>
2022-04-28 16:09:47 +0900

    Change test to kotest
    JUnit5에서 kotest로 테스트를 변경합니다.

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
 	runtimeOnly("com.h2database:h2")
 	testImplementation("io.kotest:kotest-runner-junit5:5.2.3")
 	testImplementation("io.kotest:kotest-assertions-core:5.2.3")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.1")
 	testImplementation("io.mockk:mockk:1.12.3")
 	testImplementation("com.ninja-squad:springmockk:3.1.1")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
--- src/test/kotlin/com/sangwoo/dmakerkt/controller/DMakerControllerTest.kt
+++ src/test/kotlin/com/sangwoo/dmakerkt/controller/DMakerControllerTest.kt
@@ -1,67 +1,66 @@
 package com.sangwoo.dmakerkt.controller
 
+import com.ninjasquad.springmockk.MockkBean
 import com.sangwoo.dmakerkt.dto.DeveloperDto
 import com.sangwoo.dmakerkt.service.DMakerService
 import com.sangwoo.dmakerkt.type.DeveloperLevel
 import com.sangwoo.dmakerkt.type.DeveloperSkillType
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.extensions.spring.SpringExtension
+import io.mockk.every
 import org.hamcrest.CoreMatchers.`is`
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.Test
-import org.mockito.BDDMockito.given
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.get
 import java.nio.charset.StandardCharsets
 
-@WebMvcTest
+@SpringBootTest
 @AutoConfigureMockMvc
-internal class DMakerControllerTest {
+internal class DMakerControllerTest(
+    val mockMvc: MockMvc,
+    @MockkBean val dMakerService: DMakerService
+) : DescribeSpec({
 
-    @Autowired
-    private lateinit var mockMvc: MockMvc
+    extensions(SpringExtension)
 
-    @MockBean
-    private lateinit var dMakerService: DMakerService
-
-    private val contentType = MediaType(
+    val requestContentType = MediaType(
         MediaType.APPLICATION_JSON.type,
         MediaType.APPLICATION_JSON.subtype,
         StandardCharsets.UTF_8
     )
 
-    @Test
-    fun getAllDevelopers() {
-        // given
-        val juniorDto = DeveloperDto(
-            developerSkillType = DeveloperSkillType.BACK_END,
-            developerLevel = DeveloperLevel.JUNIOR,
-            memberId = "memberId"
-        )
+    describe("DMakerController") {
+        context("When get all developers") {
+            it("Should return developers") {
+                val juniorDto = DeveloperDto(
+                    developerSkillType = DeveloperSkillType.BACK_END,
+                    developerLevel = DeveloperLevel.JUNIOR,
+                    memberId = "memberId"
+                )
 
-        val seniorDto = DeveloperDto(
-            developerSkillType = DeveloperSkillType.FRONT_END,
-            developerLevel = DeveloperLevel.SENIOR,
-            memberId = "memberId2"
-        )
+                val seniorDto = DeveloperDto(
+                    developerSkillType = DeveloperSkillType.FRONT_END,
+                    developerLevel = DeveloperLevel.SENIOR,
+                    memberId = "memberId2"
+                )
 
-        given(dMakerService.getAllEmployedDevelopers())
-            .willReturn(listOf(juniorDto, seniorDto))
+                every { dMakerService.getAllEmployedDevelopers() } returns listOf(juniorDto, seniorDto)
 
-        // when
-        // then
-        mockMvc.perform(get("/developers"))//.contentType(contentType))
-            .andExpect(status().isOk)
-            .andDo(MockMvcResultHandlers.print())
-            .andExpect(jsonPath("$.[0].developerSkillType", `is`(DeveloperSkillType.BACK_END.name)))
-            .andExpect(jsonPath("$.[0].developerLevel", `is`(DeveloperLevel.JUNIOR.name)))
-            .andExpect(jsonPath("$.[1].developerSkillType", `is`(DeveloperSkillType.FRONT_END.name)))
-            .andExpect(jsonPath("$.[1].developerLevel", `is`(DeveloperLevel.SENIOR.name)))
+                mockMvc.get("/developers") {
+                    contentType = requestContentType
+                }.andExpect {
+                    status { isOk() }
+                }.andDo {
+                    print()
+                }.andExpect {
+                    jsonPath("$.[0].developerSkillType", `is`(DeveloperSkillType.BACK_END.name))
+                    jsonPath("$.[0].developerLevel", `is`(DeveloperLevel.JUNIOR.name))
+                    jsonPath("$.[1].developerSkillType", `is`(DeveloperSkillType.FRONT_END.name))
+                    jsonPath("$.[1].developerLevel", `is`(DeveloperLevel.SENIOR.name))
+                }
+            }
+        }
     }
-}
+})
